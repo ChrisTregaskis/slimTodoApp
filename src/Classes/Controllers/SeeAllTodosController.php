fdaf9398399b904ea67efed2d1c1a3b3b82b9367
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-03-26 18:59:14 +0000

    connected DB and finished general MVC set up. SeeAllToDos request now work. Not styled

--- src/Classes/Controllers/SeeAllTodosController.php
+++ src/Classes/Controllers/SeeAllTodosController.php
@@ -3,18 +3,23 @@
 namespace TodoApp\Controllers;
 
 use Slim\Views\PhpRenderer;
+use TodoApp\Models\TodoModel;
 
 class SeeAllTodosController
 {
     private $renderer;
+    private $todoModel;
 
-    public function __construct(PHPRenderer $renderer)
+    public function __construct(PHPRenderer $renderer, TodoModel $todoModel)
     {
         $this->renderer = $renderer;
+        $this->todoModel = $todoModel;
     }
 
     public function __invoke($request, $response, $args)
     {
+        $args['allTodos'] = $this->todoModel->getAllTodos();
+
         return $this->renderer->render($response, 'alltodospage.phtml', $args);
     }
 }
--- src/Classes/Factories/SeeAllTodosControllerFactory.php
+++ src/Classes/Factories/SeeAllTodosControllerFactory.php
@@ -10,6 +10,7 @@ class SeeAllTodosControllerFactory
     public function __invoke(ContainerInterface $container)
     {
         $renderer = $container->get('renderer');
-        return new SeeAllTodosController($renderer);
+        $todoModel = $container->get('TodoModel');
+        return new SeeAllTodosController($renderer, $todoModel);
     }
 }
