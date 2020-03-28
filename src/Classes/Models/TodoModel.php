1588584f274dd2015a8d76b75520d1329f810ca6
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-03-28 19:57:24 +0000

    added docBlocks

--- src/Classes/Models/TodoModel.php
+++ src/Classes/Models/TodoModel.php
@@ -6,11 +6,21 @@ class TodoModel
 {
     private $dbConnection;
 
+    /**
+     * Instantiates TodoModel with a db connection that can be passed in each method
+     *
+     * @param \PDO $dbConnection //takes a db connection and puts it in the local variable
+     */
     public function __construct(\PDO $dbConnection)
     {
         $this->dbConnection = $dbConnection;
     }
 
+    /**
+     * method that uses db connection to gets all data from db
+     *
+     * @return array //array of all todos
+     */
     public function getAllTodos()
     {
         $query = $this->dbConnection->prepare("SELECT `id`, `item`, `complete`, `delete` FROM `todoMasterList`;");
@@ -18,6 +28,12 @@ class TodoModel
         return $query->fetchAll();
     }
 
+    /**
+     * method that uses db connection to add a new todoItem to db
+     *
+     * @param $item //takes a new todoItem
+     * @return bool //after updating db, returns true
+     */
     public function createTodo($item)
     {
         $query = $this->dbConnection->prepare("INSERT INTO `todoMasterList` (`item`) VALUES (:item)");
@@ -25,6 +41,12 @@ class TodoModel
         return $query->execute();
     }
 
+    /**
+     * method that uses db connection to set the relevant item's delete flag to true
+     *
+     * @param $id //takes id to match in db
+     * @return bool //returns true if successful
+     */
     public function deleteTodo($id)
     {
         $query = $this->dbConnection->prepare("UPDATE `todoMasterList` SET `delete` = '1' WHERE `id` = :id;");
@@ -32,6 +54,12 @@ class TodoModel
         return $query->execute();
     }
 
+    /**
+     * method that uses db connection to set the relevant item's complete flag to true
+     *
+     * @param $id //takes id to match in db
+     * @return bool //returns true if successful
+     */
     public function completeTodo($id)
     {
         $query = $this->dbConnection->prepare("UPDATE `todoMasterList` SET `complete` = '1' WHERE `id` = :id;");
@@ -39,6 +67,13 @@ class TodoModel
         return $query->execute();
     }
 
+    /**
+     * method that uses db connection to set the relevant item's complete flag to false
+     * thereby moving it to the active todoList
+     *
+     * @param $id //takes id to match in db
+     * @return bool //returns true if successful
+     */
     public function reinstateTodo($id)
     {
         $query = $this->dbConnection->prepare("UPDATE `todoMasterList` SET `complete` = '0' WHERE `id` = :id;");
@@ -46,6 +81,13 @@ class TodoModel
         return $query->execute();
     }
 
+    /**
+     * method that uses db connection to update the relevant item's description
+     *
+     * @param $item //takes description to replace with
+     * @param $id //takes id to match in db
+     * @return bool //returns true if successful
+     */
     public function editUpdateTodo($item, $id)
     {
         $query = $this->dbConnection->prepare("UPDATE `todoMasterList` SET `item` = :item WHERE `id` = :id;");
